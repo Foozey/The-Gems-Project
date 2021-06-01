2b7ad1d1e91223c367f1fcc31b352d40089b8851
Isaac <isaacfuzeallen@gmail.com>
2021-06-02 04:04:26 +1000

    Added "Hero of the Village" status effect to Emerald armor

--- src/main/java/com/foozey/gems/events/ModCommonEventsForge.java
+++ src/main/java/com/foozey/gems/events/ModCommonEventsForge.java
@@ -5,23 +5,25 @@ import com.foozey.gems.init.ModItems;
 import com.foozey.gems.util.PlayerSpawnTeleport;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = Gems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = Gems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 
-public class ModClientEventsForge {
+public class ModCommonEventsForge {
 
     // Plated Onyx Armor
     @SubscribeEvent
     public static void onVoidWithOnyx(LivingDamageEvent event) {
         if(!(event.getEntityLiving() instanceof PlayerEntity)) return;
         PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        if(player.level.isClientSide) return;
         World world = event.getEntity().level;
         if(event.getSource() == DamageSource.OUT_OF_WORLD &&
                 player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ModItems.PLATED_ONYX_HELMET.get() &&
@@ -33,23 +35,26 @@ public class ModClientEventsForge {
         }
     }
 
-    // Emerald Armor/Tools
+    // Emerald Armor
     @SubscribeEvent
-    public static void emeraldBonus(LivingEvent event) {
+    public static void emeraldArmorBonus(LivingEvent.LivingUpdateEvent event) {
         if(!(event.getEntityLiving() instanceof PlayerEntity)) return;
         PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-        if(player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ModItems.EMERALD_HELMET.get()) {
-            // TODO: Reputation increase
+        if(player.level.isClientSide) return;
+        if(player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ModItems.EMERALD_HELMET.get() &&
+                player.getItemBySlot(EquipmentSlotType.CHEST).getItem() == ModItems.EMERALD_CHESTPLATE.get() &&
+                player.getItemBySlot(EquipmentSlotType.LEGS).getItem() == ModItems.EMERALD_LEGGINGS.get() &&
+                player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ModItems.EMERALD_BOOTS.get()) {
+            player.addEffect(new EffectInstance(Effects.HERO_OF_THE_VILLAGE, 39, 0, true, false));
         }
-        if(player.getItemBySlot(EquipmentSlotType.CHEST).getItem() == ModItems.EMERALD_CHESTPLATE.get()) {
-            // TODO: Reputation increase
-        }
-        if(player.getItemBySlot(EquipmentSlotType.LEGS).getItem() == ModItems.EMERALD_LEGGINGS.get()) {
-            // TODO: Reputation increase
-        }
-        if(player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ModItems.EMERALD_BOOTS.get()) {
-            // TODO: Reputation increase
-        }
+    }
+
+    // Emerald Tools
+    @SubscribeEvent
+    public static void emeraldToolBonus(LivingEvent event) {
+        if(!(event.getEntityLiving() instanceof PlayerEntity)) return;
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        if(player.level.isClientSide) return;
         if(player.getMainHandItem().getItem() == ModItems.EMERALD_SWORD.get()) {
             // TODO: Looting buff
         }
